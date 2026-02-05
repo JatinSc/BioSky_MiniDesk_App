78cf236152a72faa1f88cd990af4d67dfcbfc9fe
jatin-gaian <jatin.s@gaiansolutions.com>
2026-02-05 22:50:13 +0530

    deployed backend url added in backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  // VITE_BASE_URL: "http://localhost:4000",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
